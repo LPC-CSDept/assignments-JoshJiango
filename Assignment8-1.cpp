beade7e2bb495b6023cd586b81e4a940df6d316c
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-09-10 02:39:37 +0000

    1st update

--- Assignment8-1.cpp
+++ Assignment8-1.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+int binarySearch(int[], int, int);
+//1st paramenter: integar array
+//2nd paramenter: size of the array N
+//3rd paramenter: target value
+//return value and type: the index of the array if the target is in the arry. -1 if not found
+
 int main() 
 {
   const int N=10;
   int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70};
+  int index;
   int first = 0;
   int last = N-1;
   int mid = (first + last)/2;
@@ -13,6 +20,23 @@ int main()
 
   //cout <<"Pleae enter the # you want to search: " << endl;
   //cin >> target;
+
+  index = binarySearch(numbers, N, target);
+  if (index == -1)
+  {
+    cout << "Not found" << endl;
+  }
+  else
+  {
+    cout << "Found at index: " << index << endl;
+  }
+}
+
+int binarySearch(int numbers[], int N, int target)
+{
+  int first = 0;
+  int last = N-1;
+  int mid = (first + last)/2;
 
   while(first <= last)
   {
@@ -30,4 +54,5 @@ int main()
       last = mid - 1;
     }
   }
+  return -1;
 }
