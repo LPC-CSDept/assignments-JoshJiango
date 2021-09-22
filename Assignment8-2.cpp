9cfd5f8d4eb74af961c7466ab500493215e86184
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-09-22 03:27:45 +0000

    initial push with some error

--- Assignment8-2.cpp
+++ Assignment8-2.cpp
@@ -1,97 +1,82 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-void makeArray(int[], int);
+void makeArray(int [], int);
 void printArray(int [], int);
-int binarySearch(int [], int, int);
-int linearSearch(int[], int, int);
+int linearSearch(int [], int , int );
+int binarySearch(int [], int , int );
 
 int main()
 {
-  const int TRY = 100;
-  const int N = 64;
-  int array[N];
-  int target;
-  int total_linear = 0;
+	const int TRY = 10000;
+	const int N = 64;
+	int array[N];
+	int target;
+	int total_linear = 0;
 
-  srand(time(0)); //set the seed number to random function
-  makeArray(array, N);
-  printArray(array, N);
+	srand(time(0)); // set the seed number to random function
+	makeArray(array, N);
+	printArray(array, N);
 
-  for(int i=0; i<TRY; i++)
-  {
-    target = array[rand() % 64];
-    total_linear += linearSearch(array, N, target);
-  }
-  cout << "The average comparison number: " << total_linear / TRY << endl;
+	for(int i=0; i<TRY; i++){
+		target = array[rand() % 64];
+		total_linear += linearSearch(array, N, target);
+	}
+	cout << "The average comparison number " << total_linear / TRY << endl;
 
-  //call any sort array function
+	// Sort array
 
-  //call binarySearch TRY times
+	// call binarySearch TRY times
+
+
+
 }
 
 int linearSearch(int array[], int N, int target)
 {
-  for(int i=0; i<N; i++)
-  {
-    if(array[i] == target)
-    {
-      return i+1;
-    }
-  }
-  return N;
+    for(int i=0; i<N; i++)
+        if ( array[i] == target)
+            return i+1;
+            
+    return N;
 }
 
 void makeArray(int array[], int N)
 {
-  for(int i=0; i<N; i++)
-  {
-    array[i] = rand()%100+1; // random numbers between 1 to 100
-  }
+	for(int i=0; i<N; i++)
+		array[i] = rand() % 100 +1 ;  // random nubers between 1 to 100
+
 }
 
 void printArray(int array[], int N)
 {
-  for(int i=0; i<N; i++)
-  {
-    cout << array[i] << "\t";
-    if(i % 8 ==7) cout << endl;
-  }
+    for(int i=0; i<N; i++)
+    {
+        cout << array[i]  << "\t" ; 
+        if (i % 8 == 7 ) cout << endl;
+    }
 }
 
-/*
 int binarySearch(int array[], int N, int target)
 {
-  int first, last, mid;
-  int cmp = 0;
+    int first, last, mid;
+    int cmp = 0;
 
-  first = 0;
-  last = 
-}
-*/
-
-int binarySearch(int numbers[], int N, int target)
-{
-  int first = 0, last = N-1;
-  
-  while(first <= last)
-  {
-    int mid = (first + last)/2;
-    if(numbers[mid] == target)
+    first = 0;
+    last = N-1;
+    while ( first <= last) 
     {
-      return mid;
+		cmp += 1;
+        mid = (first + last) / 2;
+        if ( array[mid] == target)
+            return cmp;
+        if ( array[mid] > target)
+            last = mid - 1;
+        else 
+            first = mid + 1;
     }
-    if(numbers[mid] < target)
-    {
-      first = mid +1;
-    }
-    else
-    {
-      last = mid - 1;
-    }
-  }
- return -1;
+    return cmp;
 }
