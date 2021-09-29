4ae3a9b6c8a9b176321ee0acde5bc27b2cf60943
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-09-29 02:35:02 +0000

    binarySearch function implemented

--- Assignment8-2.cpp
+++ Assignment8-2.cpp
@@ -6,42 +6,84 @@ using namespace std;
 
 void makeArray(int [], int);
 void printArray(int [], int);
+void bubbleSort(int [], int);
 int linearSearch(int [], int , int );
 int binarySearch(int [], int , int );
 
 int main()
 {
-	const int TRY = 10000;
+	const int TRY = 10;
 	const int N = 64;
 	int array[N];
 	int target;
 	int total_linear = 0;
+  int total_binary = 0;
 
 	srand(time(0)); // set the seed number to random function
 	makeArray(array, N);
 	printArray(array, N);
 
-	for(int i=0; i<TRY; i++){
+	for(int i=0; i<TRY; i++)
+  {
 		target = array[rand() % 64];
 		total_linear += linearSearch(array, N, target);
 	}
-	cout << "The average comparison number " << total_linear / TRY << endl;
+	cout << "The average comparison number with linearSearch is: " << total_linear / TRY << endl << endl;
 
 	// Sort array
+  bubbleSort(array, N);
+  printArray(array, N);
 
 	// call binarySearch TRY times
-
-
+  for(int i=0; i<TRY; i++)
+  {
+    target = array[rand() % 64];
+    //cout << target << endl;
+		total_binary += binarySearch(array, N, target);
+  }
+  cout << "The average comparison number with binarySearch is: " << total_binary / TRY << endl;
 
 }
 
 int linearSearch(int array[], int N, int target)
 {
     for(int i=0; i<N; i++)
+    {
         if ( array[i] == target)
-            return i+1;
-            
+        {
+          return i+1;
+        }
+    }
     return N;
+}
+
+int binarySearch(int array[], int N, int target)
+{
+    int first, last, mid;
+    //int cmp = 0;
+
+    first = 0;
+    last = N-1;
+    while ( first <= last) 
+    {
+		//cmp += 1;
+        mid = (first + last) / 2;
+        if (array[mid] == target)
+        {
+          //return cmp;
+          return mid;
+        }
+        if (array[mid] > target)
+        {
+          last = mid - 1;
+        }
+        else 
+        {
+          first = mid + 1;
+        }
+    }
+    //return cmp;
+    return -1;
 }
 
 void makeArray(int array[], int N)
@@ -60,23 +102,16 @@ void printArray(int array[], int N)
     }
 }
 
-int binarySearch(int array[], int N, int target)
+void bubbleSort(int array[], int N)
 {
-    int first, last, mid;
-    int cmp = 0;
-
-    first = 0;
-    last = N-1;
-    while ( first <= last) 
+  for(int i=0; i<N-1; i++)
+  {
+    for(int j=0; j<N-i-1; j++)
     {
-		cmp += 1;
-        mid = (first + last) / 2;
-        if ( array[mid] == target)
-            return cmp;
-        if ( array[mid] > target)
-            last = mid - 1;
-        else 
-            first = mid + 1;
+      if(array[j]>array[j+1])
+      {
+        swap(array[j], array[j+1]);
+      }
     }
-    return cmp;
+  }
 }
