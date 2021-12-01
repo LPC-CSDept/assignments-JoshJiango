c69248c2c054f92ca69540cf4b576e1f19a149db
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-01 00:52:51 +0000

    class, getters and setters

--- MidTermProblem2/midtermP2.hpp
+++ MidTermProblem2/midtermP2.hpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+const int NUM_STUDENTS = 10;
+const int NUM_SCORES = 3;
+
 class Student
 {
 	private:
@@ -17,6 +20,51 @@ class Student
 
   public:
     Student(){}
+
+    int getID(void) 
+    {
+      return ID;
+    }
+    string getSName(void) 
+    {
+      return sname;
+    }
+    vector<double> getScores(void) 
+    {
+      return scores;
+    }
+    double getTotalScore(void)
+    {
+      double totalScore = 0;
+      for(int i=0; i<scores.size(); i++) 
+      {
+        totalScore += scores[i];
+      }
+      return totalScore;
+    }
+    
+    void setID(int ID) 
+    {
+      this->ID = ID;
+    }
+    void setSName(string sname) 
+    {
+      this->sname = sname;
+    }
+    void setScores(vector<double> scores) 
+    {
+      this->scores = scores;
+    }
+
+    void printStudent(void)
+    {
+      cout << ID << "\t" << sname << "\t";
+      for(int i=0; i<scores.size(); i++)
+      {
+        cout << scores[i] << "\t";
+      }
+      cout << endl;
+    }
 };
 
 #endif
