479753b3b4e313ec2aa838e6e827832be8b34ce9
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-01 00:40:51 +0000

    starting midterm problem 2 making and uploading txt file for testing and some basic code to start with

---
+++ MidTermProblem2/mainmidtermP2.cpp
@@ -0,0 +1,13 @@
+#include "midtermP2.hpp"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  
+}
---
+++ MidTermProblem2/midtermP2.hpp
@@ -0,0 +1,22 @@
+#ifndef 	MIDTERMP2_H
+#define 	MIDTERMP2_H
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+class Student
+{
+	private:
+		int ID;
+    string sname;
+    vector<double> scores;
+
+  public:
+    Student(){}
+};
+
+#endif
