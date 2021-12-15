e2d3287260ef08436a548c994405bd3ba99b9664
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-15 05:00:44 +0000

    more code for course.cpp

--- FinalProblem1/Course.cpp
+++ FinalProblem1/Course.cpp
@@ -12,3 +12,32 @@ Course::Course(int id, string name, int credit)
   this->name = name;
   this->credit = credit;
 }
+
+// Getter
+int Course::getId(void)
+{
+  
+}
+string Course::getName(void)
+{
+ 
+}
+int Course::getCredit(void)
+{
+  
+}
+
+// Setter  
+void Course::setId(int id)
+{
+  
+}
+void Course::setName(string name)
+{
+  
+}
+void Course::setCredit(int credit)
+{
+  
+}
+
