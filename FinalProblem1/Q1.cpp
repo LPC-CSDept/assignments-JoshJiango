34b11a5ab995d1be26f30d7182647d5603a6c295
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-15 05:18:46 +0000

    code for read file

--- FinalProblem1/Q1.cpp
+++ FinalProblem1/Q1.cpp
@@ -5,3 +5,31 @@
 using namespace std;
 
 const int NUM_COURSES = 10;
+
+int main()
+{
+
+}
+
+void makeCourses(Course courses[]) 
+{
+  ifstream ifs;
+
+  ifs.open("courses.txt");
+  if(ifs.fail())
+  {
+    cerr << "File open error\n";
+    exit(0);
+  }
+  for(int i=0; i<NUM_COURSES; i++)
+  {
+    int id;
+    string name;
+    int credit;
+    ifs >> id >> name >> credit;
+    courses[i].setId(id);
+    courses[i].setName(name);
+    courses[i].setCredit(credit);
+  }
+  ifs.close();
+}
