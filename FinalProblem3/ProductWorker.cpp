45916153a6a077b3d94e576b4a2b800917ecda3a
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-16 07:22:52 +0000

    Constructor code for ProductWorker.cpp

--- FinalProblem3/ProductWorker.cpp
+++ FinalProblem3/ProductWorker.cpp
@@ -4,3 +4,14 @@
 #include<string>
 
 using namespace std;
+
+// Constructor
+ProductWorker::ProductWorker() { }
+ProductWorker::ProductWorker(int id, string name, string hireDate, int shift, double payRate)
+{
+  this->setId(id);
+  this->setName(name);
+  this->setHireDate(hireDate);
+  this->shift = shift;
+  this->payRate = payRate;
+}
