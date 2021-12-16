133333dbea20493e4c96634f81d26a7b43f0eff4
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-16 07:39:06 +0000

    Q3 main code update

--- FinalProblem3/Q3.cpp
+++ FinalProblem3/Q3.cpp
@@ -5,3 +5,21 @@
 using namespace std;
 
 const int NUM_WORKERS = 10;
+
+void makeWorkers(ProductWorker workers[]);
+void printWorkers(ProductWorker workers[]);
+
+int main()
+{
+
+}
+
+void makeWorkers(ProductWorker workers[]) 
+{
+
+}
+
+void printWorkers(ProductWorker workers[])
+{
+  
+}
