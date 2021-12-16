15868e241bf5c32137f2b48bfaed1030272d330c
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-16 05:46:40 +0000

    updating Employee.hpp

--- FinalProblem3/Employee.hpp
+++ FinalProblem3/Employee.hpp
@@ -3,3 +3,24 @@
 #include <string>
 
 using namespace std;
+
+class Employee
+{
+  private:
+    int id;
+    string name;
+    string hireDate;
+
+  public:
+    Employee();
+    Employee();
+    int getId();
+    string getName();
+    string getHireDate();
+    void setId();
+    void setName();
+    void setHireDate();
+    void printWorkers();
+};
+
+#endif
