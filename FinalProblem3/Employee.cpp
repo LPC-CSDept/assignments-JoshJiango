9f37043d2ebde4140b15b6da9d7120ad27b157b9
jhhroot <89234422+JoshJiango@users.noreply.github.com>
2021-12-16 06:26:32 +0000

    getter for employee.cpp

--- FinalProblem3/Employee.cpp
+++ FinalProblem3/Employee.cpp
@@ -3,3 +3,26 @@
 #include<string>
 
 using namespace std;
+
+// Constructor
+Employee::Employee() { }
+Employee::Employee(int id, string name, string hireDate)
+{
+  this->id = id;
+  this->name = name;
+  this->hireDate = hireDate;
+}
+
+// Getter
+int Employee::getId(void)
+{
+  return id;
+}
+string Employee::getName(void)
+{
+  return name;
+}
+string Employee::getHireDate(void)
+{
+  return hireDate;
+}
